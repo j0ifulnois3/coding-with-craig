a0bf062f6fc96c1e61e0fbdd2a45eba8ae535f10
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-15 09:55:59 -0500

    Feat: Basic While Loops Example

--- src/main/java/com/pluralsight/WhileExample.java
+++ src/main/java/com/pluralsight/WhileExample.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
 public class WhileExample {
+
+    public static void main(String[] args) {
+        int count = 1;
+
+        while (count<=1000){
+            System.out.println("Eat " + count + " Dounts");
+            count = count + 1;
+        }
+    }
 }
