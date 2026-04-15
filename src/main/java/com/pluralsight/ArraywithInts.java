b88ce72fc427ad236c30f1ae2cbfe2dcc517de23
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-15 13:43:18 -0500

    Feat: Basic Arrays with Ints Example

---
+++ src/main/java/com/pluralsight/ArraywithInts.java
@@ -0,0 +1,62 @@
+package com.pluralsight;
+
+public class ArraywithInts {
+    public static void main(String[] args) {
+
+        int[] numbers ={23,14,3,7};
+
+        System.out.println(numbers[3]);
+
+        for(int index = 0; index < numbers.length; index++);{
+            int number = numbers[index];
+            System.out.println(number);
+
+//        for (int index = 0; index < numbers.length; index++) {
+//            int number = numbers[index];
+//            System.out.println(number);
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
