d9ef78a5b642159fc0b4d4e6d63ce2b8134e1f65
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-15 10:26:14 -0500

    Feat: Basic For Loop Example

---
+++ src/main/java/com/pluralsight/ForLoopExample.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+
+        //Full For Loop: for (initialization; condition; other) {
+        // // code
+        //}
+        for (int count = 1; count <= 5; count++) {
+            System.out.println("Eat " + count + " Apples");
+
+
+//
+
+
+        }
+
+    }
+    }
